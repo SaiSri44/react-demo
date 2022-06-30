57081e38a3fc9ea6f26284af0da202872b14cadc
SaiSri Angajala <angajalasaisri2000@gmail.com>
2022-06-30 23:15:26 +0530

    build the static website of the Expense Tracker

---
+++ src/components/NewExpensesComponent.js
@@ -0,0 +1,31 @@
+import ExpenseItem from "./ExpenseItem";
+import "./NewExpensesComponent.css";
+
+function NewExpensesComponent(props) {
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        title={props.expensesarr[0].title}
+        amount={props.expensesarr[0].amount}
+        date={props.expensesarr[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.expensesarr[1].title}
+        amount={props.expensesarr[1].amount}
+        date={props.expensesarr[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.expensesarr[2].title}
+        amount={props.expensesarr[2].amount}
+        date={props.expensesarr[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.expensesarr[3].title}
+        amount={props.expensesarr[3].amount}
+        date={props.expensesarr[3].date}
+      ></ExpenseItem>
+    </div>
+  );
+}
+
+export default NewExpensesComponent;
