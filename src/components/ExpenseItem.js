495903f8c576f82966e352b874dde864636e4632
saisri angajala <sri.angajala911@gmail.com>
2023-09-15 10:43:17 +0530

    Made the Years Generic.Added the Rupee symbol infront of Expens.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,6 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import React from "react";
-import { useState } from "react";
 
 const ExpenseItem = (props) => {
   return (
@@ -9,7 +8,7 @@ const ExpenseItem = (props) => {
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className="expense-item__price ">{props.amount}</div>
+        <div className="expense-item__price ">₹{props.amount}</div>
       </div>
     </div>
   );
--- src/components/ExpensesFilter/ExpensesFilter.js
+++ src/components/ExpensesFilter/ExpensesFilter.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import years from "../../constants";
 import "./ExpensesFilter.css";
 
 const ExpensesFilter = (props) => {
@@ -12,10 +12,9 @@ const ExpensesFilter = (props) => {
       <div className="expenses-filter__control">
         <label>Filter by year</label>
         <select value={props.selectedYear} onChange={onChangeHandler}>
-          <option value="2022">2022</option>
-          <option value="2021">2021</option>
-          <option value="2020">2020</option>
-          <option value="2019">2019</option>
+          {years.map((year) => (
+            <option value={`${year}`}>{year}</option>
+          ))}
         </select>
       </div>
     </div>
